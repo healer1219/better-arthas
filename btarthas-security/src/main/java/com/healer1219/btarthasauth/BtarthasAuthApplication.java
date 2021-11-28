3b3894acb96461204b927b823289a94cc6676b4f
lizewei <lizewei08892@tiandy.com>
2021-11-28 16:14:01 +0800

    :beers: feat(better-arthas): create new btarthas-security

---
+++ btarthas-security/src/main/java/com/healer1219/btarthasauth/BtarthasAuthApplication.java
@@ -0,0 +1,14 @@
+package com.healer1219.btarthasauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class BtarthasAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BtarthasAuthApplication.class, args);
+    }
+
+}
---
+++ btarthas-security/src/test/java/com/healer1219/btarthasauth/BtarthasAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.healer1219.btarthasauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BtarthasAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
